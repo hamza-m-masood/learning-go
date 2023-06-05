3d3d5e23ced6d489877f98b2e8a3a8f931a28a1e
HamzaMasood1 <hamzamasood183@gmail.com>
2023-06-05 20:06:36 +0100

    simple-example

---
+++ concurrency/simple-example.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	start := time.Now()
+
+	wg.Add(2)
+	go sleep1()
+	go sleep2()
+	wg.Wait()
+
+	elapsed := time.Since(start)
+	fmt.Printf("elapsed time: %v\n", elapsed)
+}
+
+func sleep1() {
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+}
+
+func sleep2() {
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+}
