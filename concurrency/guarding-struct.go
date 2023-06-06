a412a5b091673f77c2051580bee794bc34cfa877
HamzaMasood1 <hamzamasood183@gmail.com>
2023-06-06 08:36:16 +0100

    guarding struct

---
+++ concurrency/guarding-struct.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func main() {
+	var mutex sync.Mutex
+	counter := Counter{mutex, 12}
+	counter.Increment()
+	fmt.Println(counter.value)
+}
+
+func (c *Counter) Increment() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
