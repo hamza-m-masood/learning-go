4d27fc12b6f48391eea8117e23de78b56a80e4d2
HamzaMasood1 <hamzamasood183@gmail.com>
2023-06-08 15:21:39 +0100

    once in sync package

--- concurrency/concurrency-types/cond-broadcast.go
+++ concurrency/concurrency-types/cond-broadcast.go
@@ -9,7 +9,9 @@ func main() {
 	type Button struct {
 		Clicked *sync.Cond
 	}
+
 	button := Button{Clicked: sync.NewCond(&sync.Mutex{})}
+
 	subscribe := func(c *sync.Cond, fn func()) {
 		var goroutineRunning sync.WaitGroup
 		goroutineRunning.Add(1)
@@ -22,20 +24,25 @@ func main() {
 		}()
 		goroutineRunning.Wait()
 	}
+
 	var clickRegistered sync.WaitGroup
 	clickRegistered.Add(3)
+
 	subscribe(button.Clicked, func() {
 		fmt.Println("Maximizing window.")
 		clickRegistered.Done()
 	})
+
 	subscribe(button.Clicked, func() {
 		fmt.Println("Displaying annoying dialog box!")
 		clickRegistered.Done()
 	})
+
 	subscribe(button.Clicked, func() {
 		fmt.Println("Mouse clicked.")
 		clickRegistered.Done()
 	})
+
 	button.Clicked.Broadcast()
 	clickRegistered.Wait()
 }
