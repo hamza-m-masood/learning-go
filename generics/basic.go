525675d6fb84a2a64117fe7fb14c9707a8cbec18
HamzaMasood1 <hamzamasood183@gmail.com>
2023-06-29 20:36:35 +0100

    basic generic function

---
+++ generics/basic.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+	fmt.Printf("ints: %v\nfloats: %v", SumIntsOrFloats[string, int64](ints), SumIntsOrFloats[string, float64](floats))
+}
+
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
