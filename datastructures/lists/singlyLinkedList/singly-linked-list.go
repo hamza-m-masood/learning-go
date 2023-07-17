a200d0f020f34a7c3bef810e7235d4b39043054a
HamzaMasood1 <hamzamasood183@gmail.com>
2023-07-17 09:27:22 +0100

    removing an element

--- datastructures/lists/singlyLinkedList/singly-linked-list.go
+++ datastructures/lists/singlyLinkedList/singly-linked-list.go
@@ -52,15 +52,47 @@ func (list *List) Prepend(values ...interface{}) {
 	}
 }
 
-//TODO: remove elment
+// TODO: remove elment
+func (list *List) Remove(id int) {
+	element := list.FirstElement
+	if id == 0 {
+		list.FirstElement = element.next
+		list.Size--
+
+	}
+	count := 0
+	for i := 0; i < list.Size; i++ {
+		if count == id-1 {
+			if element.next == list.LastElement {
+				list.LastElement = element
+			}
+			element.next = element.next.next
+			list.Size--
+			break
+		}
+		element = element.next
+		count++
+	}
+}
+
+// TODO: print all values of list
+func (list *List) allValues() []interface{} {
+	allValues := make([]interface{}, 0)
+	element := list.FirstElement
+	for i := 0; i < list.Size; i++ {
+		allValues = append(allValues, element.value)
+		element = element.next
+	}
+	return allValues
+}
 
 //TODO: empty (returns if list does not contain enaything)
 
 //TODO: clear (removes all elements from the list)
 
 func main() {
-	list := New(2, 3, 3)
-	list.Prepend(5, 6, 8)
-	fmt.Println(list.FirstElement.value)
-
+	list := New(2, 3, 5)
+	list.Remove(1)
+	fmt.Println(list.allValues())
+	fmt.Println(list.LastElement.value)
 }
