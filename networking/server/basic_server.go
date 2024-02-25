94428a153e1892fc010127f5964044d9bdda06f1
HamzaMasood1 <hamzamasood183@gmail.com>
2024-02-25 14:53:52 +0000

    finished making basic server

--- networking/server/basic_server.go
+++ networking/server/basic_server.go
@@ -6,16 +6,45 @@ import (
 )
 
 func Server() {
-	//creating a function outside main
-	http.HandleFunc("/", homeServe)
-	//creating a function inside the params
-	http.HandleFunc("/holiday", func(w http.ResponseWriter, _ *http.Request){
-		w.Write([]byte("holiday!!"))
-		})
+	http.HandleFunc("/", getRequest)
+	http.HandleFunc("/post", postRequest)
+	http.HandleFunc("/delete", deleteRequest)
 	http.ListenAndServe(":8080", nil)
 }
 
-func homeServe(w http.ResponseWriter, _ *http.Request) {
+func getRequest(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/"{
+		errorHandler(w, req, 404)
+		return
+	}
+	if req.Method != http.MethodGet {
+		http.Error(w, "Incorrect method", http.StatusInternalServerError)
+		return
+	}
 	fmt.Println("hello")
 	w.Write([]byte("hello"))
+}
+
+func postRequest(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "Incorrect method", http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("You just made a post request"))
+}
+
+func deleteRequest(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodDelete {
+		http.Error(w, "Incorrect method", http.StatusInternalServerError)
+		return
+	}
+	// w.Write([]byte("You just made a delete request"))
+	fmt.Fprintln(w, "You just made a delete request")
+}
+
+func errorHandler(w http.ResponseWriter, _ *http.Request, status int){
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprintln(w, "ERROR: page not found")
+	}
 }
