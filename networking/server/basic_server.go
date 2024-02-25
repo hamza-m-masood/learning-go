0148a7153f6c58edcced0a4eda13e38062702cdd
HamzaMasood1 <hamzamasood183@gmail.com>
2024-02-25 12:38:56 +0000

    basic server

--- networking/server/basic_server.go
+++ networking/server/basic_server.go
@@ -1,7 +1,23 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func Server(){
+	http.Handle("/", &home{})
+	http.Handle("/blog", &blog{})
 	http.ListenAndServe(":8080", nil)
 }
+
+type home struct{}
+type blog struct{}
+
+func (h *home) ServeHTTP(w http.ResponseWriter, req *http.Request){
+	w.Write([]byte("This is my home"))
+}
+
+func (b *blog) ServeHTTP(w http.ResponseWriter, req *http.Request){
+	w.Write([]byte("This is my blog"))
+}
+
